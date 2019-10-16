21b3bbe53af2f72ee1a7ab6b386c2b60d9a84454
Johannes Taelman <johannes.taelman@gmail.com>
2019-10-16 20:31:53 +0200

    cleanup lmntsreverb

--- modules/lmntsreverb/include.mk
+++ modules/lmntsreverb/include.mk
@@ -1,34 +1,12 @@
 #$(info include.mk)
 
-#$(info   MODULE_SRC_DIR=$(MODULE_SRC_DIR))
-#$(info   MODULE=$(MODULE))
+ALLINC += ${MODULE_SRC_DIR}/${MODULE}.h
+MODULE_DEPS += ${MODULE_BUILD_ELF}
 
-MODULE_INC=$(call sq,$(MODULE_SRC_DIR)/$(MODULE).h)
-#$(info   MODULE_INC $(MODULE_INC))
+MODULE_FILEDEPS += \
+	${MODULE_BUILD_ELF} \
+	/lib/${MODULE}.elf
 
-ALLINC+= $(MODULE_INC)
-MODULE_BDIR = $(call sq,${axoloti_home}/build/$(MODULE))
-# TODO: ensure MODULE_BDIR exists...
-trgt1:= $(call qsb,$(MODULE_BDIR))/$(MODULE).elf
-$(info trgt1 = $(trgt1))
-
-MODULEDEP := $(MODULE).elf
-MODULEDEPS += $(MODULEDEP)
-
-MODULEFILEDEPS += \
-	$(call sq,$(MODULE_BDIR)/$(MODULE).elf) \
-	$(call sq,/lib/$(MODULE).elf)
-
-#$(info MODULEDEP $(MODULEDEP))
-
-# TODO: needs cleanup
-# VPATH does not work with spaces
-VP2=$(subst $(SPACE),\ ,$(MODULE_SRC_DIR))
-#$(info VP2=$(VP2))
-
-#$(VP2)/$(MODULE).cpp $(VP2)/$(MODULE).h $(VP2)/Makefile
-#$(MODULE).elf:
-#$(call sq,$(MODULE_BDIR)/lmnts.elf):
-$(MODULE).elf:
-	@$(MKDIR_P) "$(call qs,$(MODULE_BDIR))"
-	@make -f "$(MODULE_SRC_DIR)/Makefile" -C "$(call qs,$(MODULE_BDIR))" VPATH=$(VP2) all
+${MODULE_BUILD_ELF} : ${MODULE_SRC_DIR}/*
+	@mkdir -p "${MODULE_BUILD_DIR}"
+	@make -f "${MODULE_SRC_DIR}/Makefile" -C "${MODULE_BUILD_DIR}" VPATH="${MODULE_SRC_DIR}" all
