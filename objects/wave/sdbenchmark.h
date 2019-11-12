131de8ec137d20bce42da78612291bb80bf34ca1
Johannes Taelman <johannes.taelman@gmail.com>
2019-11-12 23:32:44 +0100

    update objs

--- objects/wave/sdbenchmark.h
+++ objects/wave/sdbenchmark.h
@@ -1,5 +1,3 @@
-#include "../chibios/ext/fatfs/src/ff.h"
-#include "../chibios/os/various/chprintf.c"
 
 #define SDREADFILEPINGPONGSIZE 2048
 typedef struct {
@@ -21,11 +19,11 @@ typedef struct {
   fbuffer fbuff1;
 } sdReadFilePingpong;
 
-sdReadFilePingpong sdStreams[2] __attribute__ ((section (".sram")));
+sdReadFilePingpong sdStreams[2];
 #define PRE_SIZE (1024*1024*128)
 
 void reportFError(FRESULT err){
-      LogTextMessage("fatfs err %i\r\n",err);
+      LogTextMessage("fatfs err %i",err);
 }
 
 void BenchmarkBS(int bufsize, int nstreams){
@@ -34,7 +32,7 @@ void BenchmarkBS(int bufsize, int nstreams){
 
   // create filenames
   for(i=0;i<nstreams;i++){
-      sdReadFilePingpong *s = (sdReadFilePingpong*)(0x20000000 | (int)&sdStreams[i]);
+      sdReadFilePingpong *s = &sdStreams[i];
     s->filename[0]='x';
     s->filename[1]='0'+i/10;
     s->filename[2]='0'+i%10;
@@ -49,20 +47,24 @@ void BenchmarkBS(int bufsize, int nstreams){
   int t0 = chTimeNow();
   // open for write
   for(i=0;i<nstreams;i++){
-      sdReadFilePingpong *s = (sdReadFilePingpong*)(0x20000000 | (int)&sdStreams[i]);
+      sdReadFilePingpong *s = &sdStreams[i];
     err = f_open(&s->f, &s->filename[0], FA_WRITE | FA_CREATE_ALWAYS);
-    if (err) reportFError(err); else LogTextMessage("Open OK...");
+    if (err) reportFError(err);
+    // else LogTextMessage("Open OK...");
     err = f_lseek(&s->f, PRE_SIZE);
-    if (err) reportFError(err); else LogTextMessage("lseek1 OK...");
+    if (err) reportFError(err);
+    // else LogTextMessage("lseek1 OK...");
     err = f_lseek(&s->f, 0);
-    if (err) reportFError(err); else LogTextMessage("lseek2 OK...");
+    if (err) reportFError(err);
+    // else LogTextMessage("lseek2 OK...");
 
     s->f.cltbl = &s->clmt[0]; /* Enable fast seek feature (cltbl != NULL) */
     s->clmt[0] = SZ_TBL; /* Set table size */
     err = f_lseek(&s->f, CREATE_LINKMAP); /* Create CLMT */
     if (err) {
       LogTextMessage("lseek err %i\r\n",err);
-    }else LogTextMessage("lseek3 OK...");
+    }
+    // else LogTextMessage("lseek3 OK...");
   }
   // write
   int t1 = chTimeNow();
@@ -70,7 +72,7 @@ void BenchmarkBS(int bufsize, int nstreams){
   int j;
   for(j=0;j<nbuf;j++){
     for(i=0;i<nstreams;i++){
-      sdReadFilePingpong *s = (sdReadFilePingpong*)(0x20000000 | (int)&sdStreams[i]);
+      sdReadFilePingpong *s = &sdStreams[i];
       err = f_write(&s->f, s->fbuff0.i8buff, bufsize, &bytes_read);
       if (err) reportFError(err);
     }
@@ -78,14 +80,14 @@ void BenchmarkBS(int bufsize, int nstreams){
   int t2 = chTimeNow();
   // close
   for(i=0;i<nstreams;i++){
-    sdReadFilePingpong *s = (sdReadFilePingpong*)(0x20000000 | (int)&sdStreams[i]);
+    sdReadFilePingpong *s = &sdStreams[i];
     err = f_close(&s->f);
     if (err) reportFError(err); 
   }
   // open for read
   int t3 = chTimeNow();
   for(i=0;i<nstreams;i++){
-    sdReadFilePingpong *s = (sdReadFilePingpong*)(0x20000000 | (int)&sdStreams[i]);
+    sdReadFilePingpong *s = &sdStreams[i];
     err = f_open(&s->f, &s->filename[0], FA_READ | FA_OPEN_EXISTING);
     s->f.cltbl = &s->clmt[0]; /* Enable fast seek feature (cltbl != NULL) */
     s->clmt[0] = SZ_TBL; /* Set table size */
@@ -98,7 +100,7 @@ void BenchmarkBS(int bufsize, int nstreams){
   int t4 = chTimeNow();
   for(j=0;j<nbuf;j++){
     for(i=0;i<nstreams;i++){
-      sdReadFilePingpong *s = (sdReadFilePingpong*)(0x20000000 | (int)&sdStreams[i]);
+      sdReadFilePingpong *s = &sdStreams[i];
       err = f_read(&s->f, s->fbuff0.i8buff, bufsize, &bytes_read);
       if (err) reportFError(err); 
     }
@@ -106,19 +108,19 @@ void BenchmarkBS(int bufsize, int nstreams){
   int t5 = chTimeNow();
   // close
   for(i=0;i<nstreams;i++){
-      sdReadFilePingpong *s = (sdReadFilePingpong*)(0x20000000 | (int)&sdStreams[i]);
+      sdReadFilePingpong *s = &sdStreams[i];
     err = f_close(&s->f);
   }
   LogTextMessage("nstreams = %i, ",nstreams);
   LogTextMessage("NBUFFERS = %i, ",nbuf);
-  LogTextMessage("BUFSIZE = %i\r\n",bufsize);
-  LogTextMessage("open : %i ms\r\n",t1-t0);
-//  LogTextMessage("write : %i ms\r\n",t2-t1);
-  LogTextMessage("write BW : %i kB/s\r\n",(nstreams*nbuf*bufsize)/(t2-t1));
-  LogTextMessage("close : %i ms\r\n",t3-t2);
-  LogTextMessage("open : %i ms\r\n",t4-t3);
-//  LogTextMessage("read : %i ms\r\n",t5-t4,0);
-  LogTextMessage("read BW : %i kB/s\r\n",(nstreams*nbuf*bufsize)/(t5-t4));
+  LogTextMessage("BUFSIZE = %i",bufsize);
+  LogTextMessage("open : %i ms",t1-t0);
+//  LogTextMessage("write : %i ms",t2-t1);
+  LogTextMessage("write BW : %i kB/s",(nstreams*nbuf*bufsize)/(t2-t1));
+  LogTextMessage("close : %i ms",t3-t2);
+  LogTextMessage("open : %i ms",t4-t3);
+//  LogTextMessage("read : %i ms",t5-t4,0);
+  LogTextMessage("read BW : %i kB/s",(nstreams*nbuf*bufsize)/(t5-t4));
 }
 
 THD_FUNCTION(ThreadBenchmarkSD, arg) {
@@ -134,7 +136,7 @@ THD_FUNCTION(ThreadBenchmarkSD, arg) {
   LogTextMessage("SDCard benchmark finished.");
 }
 
-static WORKING_AREA(waThreadSD0, 1024) __attribute__ ((section (".data")));
+static WORKING_AREA(waThreadSD0, 1024);
 
 void sdbenchmark(void){
   sdStreams[0].pThreadSD = chThdCreateStatic(waThreadSD0, sizeof(waThreadSD0), NORMALPRIO, ThreadBenchmarkSD, NULL);
